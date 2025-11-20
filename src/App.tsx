d68ef6c0a916147aa8b79a2acbf0ffc1dd52df01
Heitor-Saqueto <heitorsaqueto@hotmail.com>
2025-11-20 11:38:47 -0300

    feat: adding CRUD logic

--- src/App.tsx
+++ src/App.tsx
@@ -8,17 +8,128 @@ import ItemForm from "./components/ItemForm";
 //Base URL from you Backend
 const API_BASE_URL = 'http://localhost:8080/api/v1/items';
 
+interface EditRowProps{
+  item: Item;
+  onUpdate: (updatedItem: Item) => void;
+  onCancel: ()=> void;
+}
+
+const EditRow: React.FC<EditRowProps> = ({item, onUpdate, onCancel}) =>{
+  const[name, setName] = useState(item.name);
+  const[description, setDescription] = useState(item.description);
+  const[isUpdating, setIsUpdating] = useState(false);
+  const[updateError, setUpdateError] = useState<string | null>(null);
+
+  const handleUpdate = async ()=>{
+    setUpdateError(null);
+    if(!name || !description){
+      setUpdateError("Name and description are required.");
+      return;
+    }
+
+    setIsUpdating(true);
+
+    try{
+      const updatedData = {name, description};
+      const response = await axios.put<Item>(`${API_BASE_URL}/${item.id}`,updatedData);
+
+      
+    }catch (e) {
+      console.error('Error updating item: ', e);
+      setUpdateError('Failed to update item. Check API status.');
+    } finally {
+      setIsUpdating(false);
+    }
+  };
+
+  return (
+  <div className="p-4 bg-yellow-100 rounded-md shadow-inner border border-yellow-300 space-y-2">
+      <div className="flex flex-col sm:flex-row gap-2">
+
+        <input type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        placeholder="New Name"
+        className="p-1 border border-gray-400 rounded-md flex-grow focus:ring-yellow-500 focus:border-yellow-500"
+        disabled={isUpdating} 
+        />
+
+        <input type="text" 
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+        placeholder="New Description"
+        className="p-1 border border-gray-400 rounded-md flex-grow focus:ring-yellow-500 focus:border-yellow-500"
+        disabled={isUpdating}
+        />
+
+      </div>
+
+    {updateError && <p className="text-red-600 text-sm">{updateError}</p>}
+
+    <div className="flex gap-2 justify-end">
+
+      <button
+      onClick={handleUpdate}
+      className="px-3 py-1 bg-green-600 text-white text-sm font-medium rounded-md hover:bg-green-700 disabled:opacity-50 transition duration-150"
+      disabled={isUpdating}
+      >
+
+        {isUpdating ? 'Saving...' : 'Save'}
+
+      </button>
+      <button
+      onClick={onCancel}
+      className="px-3 py-1 bg-gray-400 text-gray-800 text-sm font-medium rounded-md hover:bg-gray-500 transition duration-150"
+      disabled={isUpdating}
+      >
+      Cancel
+      </button>
+    </div>
+  </div>
+  )
+}
+
 function App(){
 
   const [items, setItems] = useState<Item[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
 
+  //new control state from each item is updating.
+  const [editingItemId, setEditingItemId] = useState<number | null>(null);
+
+
   //function: add new item to list without reload all items
   const handleItemCreated = (newItem: Item) => {
     //Add new Item to the beginning of the list
     setItems(prevItems => [newItem, ...prevItems]);
   }
+  //Updating logic(PUT)
+  const handleItemUpdate = (updatedItem: Item) => {
+    //Map a list, find an item by ID, change it.
+    setItems(prevItems => 
+      prevItems.map(item =>
+        item.id === updatedItem.id ?updatedItem : item
+      )
+    );
+    setEditingItemId(null);
+  };
+
+  //Delete logic (DELETE)
+  const handleItemDelete = async (id: number) =>{
+    if(!window.confirm(`Are you sure you want to delete item ID: ${id}? This action is irreversible.`)){
+      return;
+    }
+    try{
+    await axios.delete(`${API_BASE_URL}/${id}`);
+
+    setItems(prevItems => prevItems.filter(item => item.id !== id));
+
+   } catch (e) {
+      console.error('Error Deleting item', e);
+      setError(`Failed to delete ID ${id}. please check your backend conection.`);
+   }
+  };
 
   useEffect(()=>{
     const fetchItems = async () => {
@@ -57,7 +168,7 @@ function App(){
   return(
     <div className="container mx-auto p-4 max-w-2x1">
       <h1 className="text-3x1 font-bold text-center text-gray-800 mb-6">
-        Full Stack Portifolio Client (React + TS)
+        Full Stack CRUD (React + Spring Boot)
       </h1>
       {/*Render Forms and pass function callback*/}
       <ItemForm onItemCreated={handleItemCreated}/>
@@ -81,14 +192,46 @@ function App(){
           <ul className="space-y-3">
             {items.map (item =>(
               <li key = {item.id} className="p-3 bg-indigo-50 rounded-md shadow-sm border border-indigo-200">
-                <strong className="text-gray-900">{item.name}</strong>:
-                <span className="text-gray-600 ml-2">{item.description}</span>
-                <span className="text-xs text-indigo-400 block mt-1 ">ID: {item.id}</span>
+                {/*Conditional logic: if ID is equals edit ID , than render EditRow*/}
+                {editingItemId === item.id ? (
+                  <EditRow
+                  item={item}
+                  onUpdate={handleItemUpdate}
+                  onCancel={()=> setEditingItemId(null)}
+                  />
+                ): (
+                  <div className="flex justify-between items-center">
+                    <div>
+                   <strong className="text-gray-900">{item.name}</strong>:
+                   <span className="text-gray-600 ml-2">{item.description}</span>
+                   <span className="text-xs text-indigo-400 block mt-1 ">ID: {item.id}</span>
+                  </div>
+                  
+                  <div className="flex gap-2">
+                    {/*Btn edit, defines the state for item ID*/}
+                    <button
+                    onClick={()=> setEditingItemId(item.id)}
+                    className="px-3 py-1 bg-blue-500 text-white text-sm font-medium rounded-md hover:bg-blue-600 transition duration-150"
+                    aria-label={`Edit item ${item.name}`}
+                    >
+                     Edit
+                    </button>
+
+                    {/*Button delete*/}
+                <button 
+                  onClick={()=> handleItemDelete(item.id)}
+                  className="ml-4 px-3 py-1 bg-red-500 text-white text-sm font-medium rounded-md hover:bg-red-600 transition duration-150"
+                  aria-label={`Delete Item ${item.name}`}
+                >
+                  Delete
+                </button>
+                  </div>
+               </div> 
+                )}
               </li>
             ))}
           </ul>
-        )
-        }
+        )}
       </div>
     </div>
   );
